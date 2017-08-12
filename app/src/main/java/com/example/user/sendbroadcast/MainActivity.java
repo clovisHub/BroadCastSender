5e7be8b7ed81049ea1b8da6e54f6801710955ee1
clovis <clovajob@yahoo.com>
2017-08-12 19:52:30 -0400

    Begin SendBroadcast

---
+++ app/src/main/java/com/example/user/sendbroadcast/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.user.sendbroadcast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendBroadcast(View view){
+
+        Intent sendItent = new Intent();
+
+        sendItent.setAction("com.example.sendbroadcast");
+        sendItent.addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES);
+        sendItent.putExtra("Sent", "you");
+        sendBroadcast(sendItent); // different from your above.
+    }
+}
